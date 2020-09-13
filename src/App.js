1f243edeed94bfcfd3352bc1f5b29046e808d1e3
SakifKhan98 <sakifkhan98@gmail.com>
2020-09-13 21:09:34 +0600

    login functionality added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ import Review from "./components/Review/Review";
 import Inventory from "./components/Inventory/Inventory";
 import NotFound from "./components/NotFound/NotFound";
 import ProductDetail from "./components/productDetail/ProductDetail";
+import Shipment from "./components/Shipment/Shipment";
+import Login from "./components/Login/Login";
+import { createContext } from "react";
+import { useState } from "react";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+
+export const UserContext = createContext();
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
-      <Header></Header>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <p>Email: {loggedInUser.email}</p>
+
       <Router>
+        <Header></Header>
         <Switch>
           <Route path="/shop">
             <Shop></Shop>
@@ -21,8 +31,14 @@ function App() {
             {" "}
             <Review></Review>{" "}
           </Route>
-          <Route path="/inventory">
+          <PrivateRoute path="/inventory">
             <Inventory></Inventory>
+          </PrivateRoute>
+          <PrivateRoute path="/shipment">
+            <Shipment></Shipment>
+          </PrivateRoute>
+          <Route path="/login">
+            <Login></Login>
           </Route>
           <Route exact path="/">
             <Shop></Shop>
@@ -35,7 +51,7 @@ function App() {
           </Route>
         </Switch>
       </Router>
-    </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDH9svE6Kuqy5Ro-cs16GT_WXgwaGHNTJ0",
+  authDomain: "ema-john-simple-5b1c1.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-5b1c1.firebaseio.com",
+  projectId: "ema-john-simple-5b1c1",
+  storageBucket: "ema-john-simple-5b1c1.appspot.com",
+  messagingSenderId: "137925016389",
+  appId: "1:137925016389:web:beb415fab6335cbc551c2c",
+};
+
+export default firebaseConfig;
